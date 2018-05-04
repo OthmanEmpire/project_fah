c452b6542a3464ff1d4d2de5b78465f98e1d3544
Oz <oz.alikhan@gmail.com>
2018-05-04 19:43:50 +0300

    Initialising the project with a quickstart directory structure

---
+++ src/main/java/com/othman/fah/App.java
@@ -0,0 +1,13 @@
+package com.othman.fah;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
